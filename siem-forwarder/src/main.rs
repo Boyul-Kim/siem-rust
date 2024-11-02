fa37304c86c0934ec41865855a97ffbf3f331e42
Boyul Kim <boyulkim1@gmail.com>
2024-11-02 01:04:32 -0700

    bug fix

--- siem-forwarder/src/main.rs
+++ siem-forwarder/src/main.rs
@@ -16,23 +16,13 @@ use tokio::time::{sleep, Duration};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    // Establish a TCP connection
     let stream = TcpStream::connect("127.0.0.1:8080").await?;
-
-    // Get the OS type
     let os_type: &str = env::consts::OS;
 
-    // Define the logging function based on OS
     match os_type {
-        "windows" => {
-            log_windows_os(stream).await?;
-        }
-        "linux" => {
-            log_linux_os(stream).await?;
-        }
-        "macos" => {
-            log_mac_os(stream).await?;
-        }
+        "windows" => log_windows_os(stream).await?,
+        "linux" => log_linux_os(stream).await?,
+        "macos" => log_mac_os(stream).await?,
         _ => {
             return Err(Box::new(std::io::Error::new(
                 std::io::ErrorKind::Unsupported,
@@ -40,7 +30,6 @@ async fn main() -> Result<(), Box<dyn Error>> {
             )) as Box<dyn Error>);
         }
     }
-
     Ok(())
 }
 
@@ -48,9 +37,12 @@ fn open_windows_event_log() -> Result<HANDLE, Box<dyn Error>> {
     let log_name: Vec<u16> = OsStr::new("System")
         .encode_wide()
         .chain(Some(0))
-        .collect::<Vec<u16>>();
+        .collect();
     let log_name_ptr: PCWSTR = PCWSTR(log_name.as_ptr());
     let log_handle: HANDLE = unsafe { OpenEventLogW(None, log_name_ptr)? };
+    if log_handle.is_invalid() {
+        return Err(Box::new(WinError::from_win32()));
+    }
     Ok(log_handle)
 }
 
@@ -73,7 +65,6 @@ struct WrapperEventlogRecord {
     data_offset: u32,
 }
 
-// Implement conversion from EventlogRecord to WrapperEventlogRecord
 impl From<EVENTLOGRECORD> for WrapperEventlogRecord {
     fn from(orig: EVENTLOGRECORD) -> Self {
         WrapperEventlogRecord {
@@ -98,8 +89,8 @@ impl From<EVENTLOGRECORD> for WrapperEventlogRecord {
 
 async fn read_event_log(log_handle: HANDLE, mut stream: TcpStream) -> Result<u32, Box<dyn Error>> {
     let mut buffer: Vec<u8> = vec![0u8; 4096];
-    let mut bytes_read: u32 = 0u32;
-    let mut min_number_of_bytes_needed: u32 = 0u32;
+    let mut bytes_read: u32 = 0;
+    let mut min_number_of_bytes_needed: u32 = 0;
 
     let mut oldest_record: u32 = 0;
     let mut total_records: u32 = 0;
@@ -114,11 +105,11 @@ async fn read_event_log(log_handle: HANDLE, mut stream: TcpStream) -> Result<u32
 
     unsafe {
         loop {
-            let read_flags: READ_EVENT_LOG_READ_FLAGS = READ_EVENT_LOG_READ_FLAGS(EVENTLOG_SEEK_READ.0 | EVENTLOG_FORWARDS_READ);
+            let read_flags = READ_EVENT_LOG_READ_FLAGS(EVENTLOG_SEQUENTIAL_READ.0 | EVENTLOG_FORWARDS_READ);
             let success: Result<(), WinError> = ReadEventLogW(
                 log_handle,
                 read_flags,
-                last_record_read + 1,
+                last_record_read + 1, // Start from the beginning for testing
                 buffer.as_mut_ptr() as *mut _,
                 buffer.len() as u32,
                 &mut bytes_read,
@@ -126,112 +117,54 @@ async fn read_event_log(log_handle: HANDLE, mut stream: TcpStream) -> Result<u32
             );
 
             if success.is_err() {
-                let error_code: windows::Win32::Foundation::WIN32_ERROR = GetLastError();
+                let error_code = GetLastError();
                 if error_code == ERROR_INSUFFICIENT_BUFFER {
                     buffer.resize(min_number_of_bytes_needed as usize, 0);
+                    continue;
                 } else if error_code.to_hresult().to_string() == "0x80070026" {
-                    // println!("reached the end of the file");
                     sleep(Duration::from_secs(1)).await;
                     continue;
                 } else {
-                    // Handle other errors
-                    return Err(Box::new(WinError::from_win32()) as Box<dyn Error>);
+                    return Err(Box::new(WinError::from_win32()));
                 }
             }
 
-            let mut offset: usize = 0;
+            let mut offset = 0;
             while offset < bytes_read as usize {
                 let record_ptr: *const EVENTLOGRECORD = buffer.as_ptr().add(offset) as *const EVENTLOGRECORD;
                 let record: &EVENTLOGRECORD = &*record_ptr;
 
-                let mut writer = BufWriter::new(&mut stream);
-                
                 let wrapper_event_log_record: WrapperEventlogRecord = record.clone().into();
                 let serialized_data = bincode::serialize(&wrapper_event_log_record)?;
                 let data_len = (serialized_data.len() as u32).to_be_bytes();
-                //ensures that both the length and the data are sent together for each record
+                let mut writer = BufWriter::new(&mut stream);
                 writer.write_all(&data_len).await?;
                 writer.write_all(&serialized_data).await?;
                 writer.flush().await?;
-                last_record_read = record.RecordNumber;
                 offset += record.Length as usize;
-               
             }
             sleep(Duration::from_secs(1)).await;
         }
-    };
-
+    }
     Ok(bytes_read)
 }
 
 async fn log_windows_os(stream: TcpStream) -> Result<(), Box<dyn Error>> {
     let log_handle: HANDLE = open_windows_event_log()?;
     let bytes_read: u32 = read_event_log(log_handle, stream).await?;
-
     println!("Read {} bytes from the event log", bytes_read);
-
     unsafe {
-        let _ = CloseEventLog(log_handle);
-    };
-
+        CloseEventLog(log_handle);
+    }
     Ok(())
 }
 
-async fn log_linux_os(stream: TcpStream) -> Result<(), Box<dyn Error>> {
+async fn log_linux_os(_stream: TcpStream) -> Result<(), Box<dyn Error>> {
     println!("RUNNING LINUX");
-
     Ok(())
 }
 
-async fn log_mac_os(stream: TcpStream) -> Result<(), Box<dyn Error>> {
+async fn log_mac_os(_stream: TcpStream) -> Result<(), Box<dyn Error>> {
     println!("RUNNING MAC");
-
     Ok(())
 }
-
-
- // let record_ptr: *const EVENTLOGRECORD = buffer.as_ptr().add(offset) as *const EVENTLOGRECORD;
-                // let record: &EVENTLOGRECORD = &*record_ptr;
-
-                // let event_id: u32 = record.EventID & 0xFFFF_FFFF;
-                // let record_number: u32 = record.RecordNumber;
-                // let time_generated: u32 = record.TimeGenerated;
-
-                // let datetime: std::time::SystemTime = UNIX_EPOCH + Duration::from_secs(time_generated as u64);
-                // let datetime: chrono::DateTime<chrono::Local> = datetime.into();
-                // let time_generated_str: String = datetime.format("%Y-%m-%d %H:%M:%S").to_string();
-
-                // let source_name_ptr: *const u16 = (record_ptr as *const u8).add(size_of::<EVENTLOGRECORD>()) as *const u16;
-                // let source_name: String = {
-                //     let mut len: usize = 0;
-                //     while *source_name_ptr.add(len) != 0 {
-                //         len += 1;
-                //     }
-                //     let slice: &[u16] = std::slice::from_raw_parts(source_name_ptr, len);
-                //     String::from_utf16_lossy(slice)
-                // };
-
-                // let computer_name_ptr: *const u16 = source_name_ptr.add(source_name.len() + 1);
-                // let computer_name: String = {
-                //     let mut len: usize = 0;
-                //     while *computer_name_ptr.add(len) != 0 {
-                //         len += 1;
-                //     }
-                //     let slice: &[u16] = std::slice::from_raw_parts(computer_name_ptr, len);
-                //     String::from_utf16_lossy(slice)
-                // };
-
-                // println!("Event ID: {}", event_id);
-                // println!("Record Number: {}", record_number);
-                // println!("Time Generated: {}", time_generated_str);
-                // println!("Source Name: {}", source_name);
-                // println!("Computer Name: {}", computer_name);
-                // println!(" ");
-                // println!("sending...");
-                // let data = "finished\n";
-                // let mut writer = BufWriter::new(&mut stream);
-
-                // writer.write_all(data.as_bytes()).await?;
-                // writer.flush().await?;
-                // // stream.write_all(b"finished").await?;
-                // offset += record.Length as usize;
